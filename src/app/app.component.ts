9cdf98906841004b4541aa013b4a77dc954d9580
unknown <31121986g@gmail.com>
2019-06-08 12:11:00 +0530

    learning custom directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practice-june08';
+
+  numbers: number[] = [1, 2, 3, 4];
+
 }
---
+++ src/app/sethighlight.directive.ts
@@ -0,0 +1,36 @@
+import { Directive, HostBinding, ElementRef, Renderer2, OnInit, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appSethighlight]'
+})
+export class SethighlightDirective implements OnInit {
+
+  @HostBinding('innerText') innerTextValue: string = '1';
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+  }
+
+  ngOnInit() {
+
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+
+  }
+
+  @HostListener('mouseover') onMouseOverCustom() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    console.log(this.innerTextValue);
+  }
+
+  @HostListener('mouseleave') onMouseOutCustom() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
+  }
+
+  @HostListener('mousedown') onMouseDownCustom() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'red');
+  }
+
+  @HostListener('mouseup') onMouseUpCustom() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green');
+  }
+
+}
